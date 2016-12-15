cf6822a02656ec9bf5c17725113d2f5d301e35fa
nemoNoboru <felipetavres@gmail.com>
2016-12-15 13:13:05 +0100

    added design card creator

---
+++ app/src/main/java/cyanoboru/grabmycard/cardCreator.java
@@ -0,0 +1,13 @@
+package cyanoboru.grabmycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cardCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_creator);
+    }
+}
